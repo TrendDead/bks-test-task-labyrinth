de178892590f2cb9e62b6a11dee3fea7679fe134
git - пше <89956045+TrendDead@users.noreply.github.com>
2023-03-07 06:16:13 +0700

    Рефакторинг

--- Assets/ProjectResurses/Scripts/Controllers/PlayerController.cs
+++ Assets/ProjectResurses/Scripts/Controllers/PlayerController.cs
@@ -4,9 +4,28 @@ using System;
 /// <summary>
 /// Контроллер игрока
 /// </summary>
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public Action EndLevel = delegate { };
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Правка прохождения шара сквозь стен
+    /// </summary>
+    private void Update()
+    {
+        if(_rigidbody.velocity == Vector3.zero)
+        {
+            _rigidbody.velocity = Vector3.up * 0.1f;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
